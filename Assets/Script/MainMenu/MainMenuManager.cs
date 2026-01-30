40c9c28b6bf79dc67caa007ffe5a1db635e93728
Mas Persian <muhammedafnans121@gmail.com>
2026-01-31 02:21:07 +0530

    MainMenu
    Mainmenu Updating

---
+++ Assets/Script/MainMenu/MainMenuManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SceneChanging(int SceneNumber)
+    {
+        SceneManager.LoadScene(SceneNumber);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
